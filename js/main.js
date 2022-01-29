657a8626bab65af5820f9a8db632656c53aeb6fb
mariananghel05 <mariananghel99@gmail.com>
2022-01-30 01:59:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 var paint = new Paint(document.querySelector("#canvas"));
 paint.setCanvasAutoresize(true, 1, 0.94);
+var penclick = false;
 
 var pen = false;
 document.querySelector('#pen').addEventListener('click', ()=>{
+    penclick = true;
     pen = !pen;
     if(pen)
         paint.pen();
     else
         paint.removeCanvasEvents();
+
+        console.log(paint.eventFunctions)
     });
 
 document.querySelector('#undo').addEventListener('click', ()=>{
@@ -59,4 +63,35 @@ function tool(){
     list.push(container);
     list.push(thick)
     list.push(color)
+}
+
+
+setTimeout(()=>{
+    document.querySelector('p').textContent = "Press pen icon to draw!";
+}, 2000)
+
+
+
+var pen23 = document.querySelector('#pen');
+pen23.addEventListener('click', check);
+var text = document.getElementById('text');
+
+
+function check(){
+    setTimeout(()=>{
+        if(!penclick)
+            check
+        else{
+            pen23.removeEventListener('click', check);
+            text.textContent = "Yeah! just like that.";
+            setTimeout(()=>{
+                text.textContent = "Ok, Now have some fun with it! I will go silent.";
+                setTimeout(()=>{
+                    document.querySelector('#text').remove();
+                }, 6000)
+
+            }, 2000);
+        }
+
+    }, 200);
 }
